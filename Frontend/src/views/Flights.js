dfc0c814792236aa0ab168c7ff015f3fc25e9809
Almeida-a <aralmeida99@gmail.com>
2021-05-28 17:32:31 +0100

    Added first version of frontend

---
+++ Frontend/src/views/Flights.js
@@ -0,0 +1,147 @@
+import React from "react";
+// react plugin for creating notifications over the dashboard
+import NotificationAlert from "react-notification-alert";
+// react-bootstrap components
+import {
+  Alert,
+  Badge,
+  Button,
+  Card,
+  Modal,
+  Navbar,
+  Nav,
+  Table,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+
+function Flights() {
+
+  const notificationAlertRef = React.useRef(null);
+  const notify = (place) => {
+    var color = Math.floor(Math.random() * 5 + 1);
+    var type;
+    switch (color) {
+      case 1:
+        type = "primary";
+        break;
+      case 2:
+        type = "success";
+        break;
+      case 3:
+        type = "danger";
+        break;
+      case 4:
+        type = "warning";
+        break;
+      case 5:
+        type = "info";
+        break;
+      default:
+        break;
+    }
+    var options = {};
+    options = {
+      place: place,
+      message: (
+        <div>
+          <div>
+            <b>Notifications activated!</b>
+          </div>
+        </div>
+      ),
+      type: type,
+      icon: "nc-icon nc-bell-55",
+      autoDismiss: 7,
+    };
+    notificationAlertRef.current.notificationAlert(options);
+  };
+  
+
+  return (
+    <>
+      <div className="rna-container">
+        <NotificationAlert ref={notificationAlertRef} />
+      </div>
+      <Container fluid>
+        <Row>
+          <Col md="12">
+            <Card className="strpied-tabled-with-hover">
+              <Card.Header>
+                <Card.Title as="h4">Flights</Card.Title>
+                <p className="card-category">
+                  To Lisbon Airport
+                </p>
+              </Card.Header>
+              <Card.Body className="table-full-width table-responsive px-0">
+                <Table className="table-hover table-striped">
+                  <thead>
+                    <tr>
+                      <th className="border-0">ID</th>
+                      <th className="border-0">From</th>
+                      <th className="border-0">Arrival Time</th>
+                      <th className="border-0">Notify</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>London</td>
+                      <td>11:23h</td>
+                      <Button block onClick={() => notify("tl")} variant="info">
+                        Notify
+                      </Button>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Berlin</td>
+                      <td>11:45h</td>
+                      <Button block onClick={() => notify("tl")} variant="info">
+                        Notify
+                      </Button>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>Paris</td>
+                      <td>12:15h</td>
+                      <Button block onClick={() => notify("tl")} variant="info">
+                        Notify
+                      </Button>
+                    </tr>
+                    <tr>
+                      <td>4</td>
+                      <td>Madrid</td>
+                      <td>12:40h</td>
+                      <Button block onClick={() => notify("tl")} variant="info">
+                        Notify
+                      </Button>
+                    </tr>
+                    <tr>
+                      <td>5</td>
+                      <td>Birmingham</td>
+                      <td>13:00h</td>
+                      <Button block onClick={() => notify("tl")} variant="info">
+                        Notify
+                      </Button>
+                    </tr>
+                    <tr>
+                      <td>6</td>
+                      <td>Moscow</td>
+                      <td>13:35h</td>
+                      <Button block onClick={() => notify("tl")} variant="info">
+                        Notify
+                      </Button>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default Flights;
---
+++ Frontend/src/views/Record.js
@@ -0,0 +1,92 @@
+import React from "react";
+// react plugin for creating notifications over the dashboard
+import NotificationAlert from "react-notification-alert";
+// react-bootstrap components
+import {
+  Alert,
+  Badge,
+  Button,
+  Card,
+  Modal,
+  Navbar,
+  Nav,
+  Table,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
+
+function Flights() {
+    return (
+        <>
+          <Container fluid>
+            <Row>
+              <Col md="12">
+                <Card className="strpied-tabled-with-hover">
+                  <Card.Header>
+                    <Card.Title as="h4">Flights</Card.Title>
+                    <p className="card-category">
+                      From Lisbon Airport
+                    </p>
+                  </Card.Header>
+                  <Card.Body className="table-full-width table-responsive px-0">
+                    <Table className="table-hover table-striped">
+                      <thead>
+                        <tr>
+                          <th className="border-0">ID</th>
+                          <th className="border-0">To</th>
+                          <th className="border-0">Departure Time</th>
+                          <th className="border-0">Arrival Time</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr
+                            href="" // Why is it that href is not recognized?
+                          >
+                          <td>1</td>
+                          <td>London</td>
+                          <td>08:23h</td>
+                          <td>11:23h</td>
+                        </tr>
+                        <tr>
+                          <td>2</td>
+                          <td>Berlin</td>
+                          <td>08:45h</td>
+                          <td>11:45h</td>
+                        </tr>
+                        <tr>
+                          <td>3</td>
+                          <td>Paris</td>
+                          <td>09:15h</td>
+                          <td>12:15h</td>
+                        </tr>
+                        <tr>
+                          <td>4</td>
+                          <td>Madrid</td>
+                          <td>09:40h</td>
+                          <td>12:40h</td>
+                        </tr>
+                        <tr>
+                          <td>5</td>
+                          <td>Birmingham</td>
+                          <td>10:00h</td>
+                          <td>13:00h</td>
+                        </tr>
+                        <tr>
+                          <td>6</td>
+                          <td>Moscow</td>
+                          <td>10:35h</td>
+                          <td>13:35h</td>
+                        </tr>
+                      </tbody>
+                    </Table>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </>
+      )
+}
+
+export default Flights;
