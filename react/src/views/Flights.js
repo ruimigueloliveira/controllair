94873964815aaa6b131df9026984860767edea02
Almeida-a <aralmeida99@gmail.com>
2021-06-04 17:18:32 +0100

    More attempts at displaying backend info (map function)

--- react/src/views/Flights.js
+++ react/src/views/Flights.js
@@ -18,6 +18,9 @@ import {
 // Using axios
 import axios from 'axios'
 
+const ARRIVALS_URL = `http://localhost:8080/api/arrivals`;
+const dynamicData = [axios.get(ARRIVALS_URL)];
+
 function Flights() {
 
   const notificationAlertRef = React.useRef(null);
@@ -59,12 +62,20 @@ function Flights() {
     };
     notificationAlertRef.current.notificationAlert(options);
   };
+  // const dynamicData = {
+    
+  // };
   
   //------------------------------GET-DATA-FROM-API------------------------------
-  // TODO: allow cors
   // axios.get("http://localhost:8080/api/arrivals")
   // .then(
   //   console.log("Great success!")
+  // )
+  // .then(
+  //   res => {
+  //     const flights = res.data;
+  //     this.setState({flights})
+  //   }
   // )
   // ----------------------------------------------------------------------------
   
@@ -84,72 +95,140 @@ function Flights() {
                 </p>
               </Card.Header>
               <Card.Body className="table-full-width table-responsive px-0">
-                <Table className="table-hover table-striped">
-                  <thead>
-                    <tr>
-                      <th className="border-0">ID</th>
-                      <th className="border-0">From</th>
-                      <th className="border-0">Arrival Time</th>
-                      <th className="border-0">Notify</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>London</td>
-                      <td>11:23h</td>
-                      <Button block onClick={() => notify("tl")} variant="info">
-                        Notify
-                      </Button>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Berlin</td>
-                      <td>11:45h</td>
-                      <Button block onClick={() => notify("tl")} variant="info">
-                        Notify
-                      </Button>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>Paris</td>
-                      <td>12:15h</td>
-                      <Button block onClick={() => notify("tl")} variant="info">
-                        Notify
-                      </Button>
-                    </tr>
-                    <tr>
-                      <td>4</td>
-                      <td>Madrid</td>
-                      <td>12:40h</td>
-                      <Button block onClick={() => notify("tl")} variant="info">
-                        Notify
-                      </Button>
-                    </tr>
-                    <tr>
-                      <td>5</td>
-                      <td>Birmingham</td>
-                      <td>13:00h</td>
-                      <Button block onClick={() => notify("tl")} variant="info">
-                        Notify
-                      </Button>
-                    </tr>
-                    <tr>
-                      <td>6</td>
-                      <td>Moscow</td>
-                      <td>13:35h</td>
-                      <Button block onClick={() => notify("tl")} variant="info">
-                        Notify
-                      </Button>
-                    </tr>
-                  </tbody>
-                </Table>
+                <FlightsTable></FlightsTable>
               </Card.Body>
             </Card>
           </Col>
         </Row>
       </Container>
     </>
+  )
+}
+
+function FlightsTable({ res }) {
+
+  const notificationAlertRef = React.useRef(null);
+  const notify = (place) => {
+    var color = Math.floor(Math.random() * 5 + 1);
+    var type;
+    switch (color) {
+      case 1:
+        type = "primary";
+        break;
+      case 2:
+        type = "success";
+        break;
+      case 3:
+        type = "danger";
+        break;
+      case 4:
+        type = "warning";
+        break;
+      case 5:
+        type = "info";
+        break;
+      default:
+        break;
+    }
+    var options = {};
+    options = {
+      place: place,
+      message: (
+        <div>
+          <div>
+            <b>Notifications activated!</b>
+          </div>
+        </div>
+      ),
+      type: type,
+      icon: "nc-icon nc-bell-55",
+      autoDismiss: 7,
+    };
+    notificationAlertRef.current.notificationAlert(options);
+  };
+
+  // const dynamicData = [
+  //   <tr>
+  //     <td>{res.data.estDepartureAirport}</td>
+  //     <td>London</td>
+  //     <td>11:23h</td>
+  //     <Button block onClick={() => notify("tl")} variant="info">
+  //       Notify
+  //     </Button>  
+  //   </tr>
+  // ];
+  
+  return (
+    <Table className="table-hover table-striped">
+      <thead>
+        <tr>
+          <th className="border-0">ID</th>
+          <th className="border-0">From</th>
+          <th className="border-0">Arrival Time</th>
+          <th className="border-0">Notify</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* {{ dynamicData }} */}
+        {dynamicData.map((flight) =>
+          <tr key={flight}>
+          {flight.map((key, obj) =>
+            <td key={key}>{
+              {obj}
+              }</td>
+          )}
+          </tr>
+        )}
+        <tr>
+          <td>1</td>
+          <td>London</td>
+          <td>11:23h</td>
+          <Button block onClick={() => notify("tl")} variant="info">
+            Notify
+          </Button>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Berlin</td>
+          <td>11:45h</td>
+          <Button block onClick={() => notify("tl")} variant="info">
+            Notify
+          </Button>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Paris</td>
+          <td>12:15h</td>
+          <Button block onClick={() => notify("tl")} variant="info">
+            Notify
+          </Button>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Madrid</td>
+          <td>12:40h</td>
+          <Button block onClick={() => notify("tl")} variant="info">
+            Notify
+          </Button>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>Birmingham</td>
+          <td>13:00h</td>
+          <Button block onClick={() => notify("tl")} variant="info">
+            Notify
+          </Button>
+        </tr>
+        <tr>
+          <td>6</td>
+          <td>Moscow</td>
+          <td>13:35h</td>
+          <Button block onClick={() => notify("tl")} variant="info">
+            Notify
+          </Button>
+        </tr>
+      </tbody>
+    </Table>
   )
 }
 
