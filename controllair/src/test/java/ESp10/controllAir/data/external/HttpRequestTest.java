1ebb0f7837d175d646f9395936941fc18dfcbe29
António Domingues <antonioccdomingues@hotmail.com>
2021-06-07 15:25:42 +0100

    tests

--- controllair/src/test/java/ESp10/controllAir/data/external/HttpRequestTest.java
+++ controllair/src/test/java/ESp10/controllAir/data/external/HttpRequestTest.java
@@ -2,6 +2,7 @@ package ESp10.controllAir.data.external;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -14,9 +15,10 @@ public class HttpRequestTest {
 
   @Autowired private TestRestTemplate restTemplate;
 
-  // public void flightSouldReturnDefaultMessage() {
-  //   assertThat(
-  //       this.restTemplate.getForObject(
-  //           "http://localhost:" + port + "/api/arrivals", OpenSkyDto[].class));
-  // }
+  @Test
+  public void flightSouldReturnDefaultMessage() {
+    assertThat(
+        this.restTemplate.getForObject(
+            "http://localhost:" + port + "/api/arrivals", OpenSkyDto[].class));
+  }
 }
