ec04039f5f71c7bbbd2e9a84249d63c0c1ef2851
Almeida-a <aralmeida99@gmail.com>
2021-06-09 12:04:11 +0100

    Some auto cleanup...

--- controllair/src/test/java/ESp10/controllAir/data/external/HttpRequestTest.java
+++ controllair/src/test/java/ESp10/controllAir/data/external/HttpRequestTest.java
@@ -1,7 +1,5 @@
 package ESp10.controllAir.data.external;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
