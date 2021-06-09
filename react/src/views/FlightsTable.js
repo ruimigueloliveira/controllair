f4afb46bc4070c55ec66e4b361ecedf866367291
Almeida-a <aralmeida99@gmail.com>
2021-06-09 17:32:46 +0100

    State for storing notifiable flights

--- react/src/views/FlightsTable.js
+++ react/src/views/FlightsTable.js
@@ -11,7 +11,8 @@ class FlightsTable extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            arrivalsData:[]
+            arrivalsData:[],
+            notifiable_flights:[], // This state needs to go to parent components for persistency (TODO later)
         }
     }
 
@@ -23,11 +24,12 @@ class FlightsTable extends React.Component{
     }
     
     render() {
+
         return (
             <Table className="table-hover table-striped">
                 <thead>
                 <tr>
-                    <th className="border-0">Icao24</th>
+                    <th className="border-0">ICAO</th>
                     <th className="border-0">From</th>
                     <th className="border-0">Time first seen</th>
                     <th className="border-0">Time last seen</th>
@@ -44,9 +46,10 @@ class FlightsTable extends React.Component{
                             <td>{flight.firstSeen}</td>
                             <td>{flight.lastSeen}</td>
                             <Button block onClick={() => {
-                                this.props.notify("tl", "Notifications activated for flight: "+flight.callsign+"!");
+                                this.props.notify("tl", "Notifications activated for flight: "+flight.icao24+"!");
+                                this.state.notifiable_flights.push(flight.icao24)
                                 }} variant="info">
-                            Notify
+                                Notify
                             </Button>
                         </tr>
                     )
