a5d5efa557f7dac50f8d5836921c1115e464ea14
Henrique Silva <henrique>
2021-06-10 22:00:25 +0100

    mapa

--- react/src/views/Maps.js
+++ react/src/views/Maps.js
@@ -18,6 +18,13 @@ export class Maps extends Component {
     //The onMarkerClick method is used to show the InfoWindow, which is a component
     //in the google-maps-react library which gives you the ability for a pop-up
     //window showing details of the clicked Marker
+
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {},
+    };
+
     onMarkerClick = (props, marker, e) =>
         this.setState({
           selectedPlace: props,
@@ -27,7 +34,7 @@ export class Maps extends Component {
 
     //The onClose method is for closing the InfoWindow once a user clicks on
     //the close button on the InfoWindow.
-    onClose = props => {
+    onMapClicked = (props) => {
         if (this.state.showingInfoWindow) {
           this.setState({
             showingInfoWindow: false,
@@ -55,53 +62,48 @@ export class Maps extends Component {
           });
     }
 
-    render() {
+    renderPlanes(){
+        var count = 0;
+        console.log("3253465y")
+        console.log(this.state.arrivalsData)
+        return this.state.arrivalsData.map(flight => {
+            count += 1;
+            return <Marker
+                onClick = { this.onMarkerClick }
+                title = { "Airplane: " + count }
+                position={{
+                  lat: getRandomArbitrary(38.76450596898016, 38.78761708928799),
+                  lng: getRandomArbitrary(-9.144989107270954, -9.12991438259596)
+                }}
+                name={"Voo id: "+ flight.icao24 + ", Plane Chassis: " + flight.callsign + ", Departure Airport: " + flight.estDepartureAirport + ", Last Seen: " + flight.lastSeen}
+                icon={{
+                      url: 'https://pics.clipartpng.com/Airplane_PNG_Clipart-421.png',
+                      scaledSize: new google.maps.Size(30, 30)
+                }}
+            />
+        })
+    }
 
+    render(){
 
-        return (
-          <Map
-            google={this.props.google}
-            zoom={14}
-            style={mapStyles}
-            initialCenter={
-              {
-                lat: 38.775275699356314,
-                lng: -9.135843353807045
-              }
-            }
-          >
-            
-            {
-              this.state.arrivalsData.map(
-                flight =>
-                        <tr>
-                          <Marker
-                            onClick={this.onMarkerClick}
-                            position={{
-                              lat: getRandomArbitrary(38.76450596898016, 38.78761708928799),
-                              lng: getRandomArbitrary(-9.144989107270954, -9.12991438259596)
-                            }}
-                            name={"Voo id: "+ flight.icao24 + ", Plane Chassis: " + flight.callsign + ", Departure Airport: " + flight.estDepartureAirport + ", Last Seen: " + flight.lastSeen}
-                          />
-                          <InfoWindow
-                            marker={this.state.activeMarker}
-                            visible={this.state.showingInfoWindow}
-                            onClose={this.onClose}
-                          >
-                            <div>
-                              <h4>
-                                {this.state.selectedPlace.name}
-                              </h4>
-                            </div>
-                          </InfoWindow>
-                        </tr>
-                )
-            }
-            
-          </Map>
-
+        return(
+            <Map
+                google={this.props.google}
+                zoom={14}
+                style={mapStyles}
+                initialCenter={
+                  {
+                    lat: 38.775275699356314,
+                    lng: -9.135843353807045
+                  }
+                }
+                onClick={this.onMapClicked}
+            >
+                <div>{ this.renderPlanes() } </div>
+            </Map>
         );
-      }
+    }
+
 }
 
 function getRandomArbitrary(min, max) {
